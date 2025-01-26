c39bc5a7b4d13e558252ef33cf6f0dff22ca90ce
GiancarloJr <108488305+GiancarloJr@users.noreply.github.com>
2025-01-26 18:11:41 -0300

    update footer and services

--- src/app/services/services.component.ts
+++ src/app/services/services.component.ts
@@ -8,4 +8,59 @@ import { Component, ViewEncapsulation } from '@angular/core';
 })
 export class ServicesComponent {
 
+  images = [
+    { src: 'https://cdn-icons-png.flaticon.com/512/13027/13027079.png', alt: 'Empacotadores para Supermercados', title: 'Supermercados' },
+    { src: 'https://cdn-icons-png.flaticon.com/512/4466/4466877.png', alt: 'Fiscal de Obras', title: 'Fiscal de Obras' },
+    { src: 'https://cdn-icons-png.flaticon.com/512/4221/4221310.png', alt: 'Higienização', title: 'Higienização' },
+    { src: 'https://cdn-icons-png.flaticon.com/512/291/291405.png', alt: 'Limpeza Mecanizada', title: 'Limpeza Mecanizada' },
+    { src: 'https://cdn-icons-png.flaticon.com/512/3130/3130472.png', alt: 'Locação de Veículos', title: 'Locação de Veículos' },
+    { src: 'https://cdn-icons-png.flaticon.com/512/6680/6680546.png', alt: 'Motoristas', title: 'Motoristas' },
+    { src: 'https://cdn-icons-png.flaticon.com/512/6680/6680546.png', alt: 'Motoristas', title: 'Motoristas' },
+    { src: 'https://cdn-icons-png.flaticon.com/512/6680/6680546.png', alt: 'Motoristas', title: 'Motoristas' }
+  ];
+
+  visibleImages = this.images.slice(0, 4); // Inicia mostrando as primeiras 4 imagens
+  currentIndex = 0;
+  interval: any;
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.startAutoSlide();
+  }
+
+  ngOnDestroy(): void {
+    if (this.interval) {
+      clearInterval(this.interval);
+    }
+  }
+
+  startAutoSlide(): void {
+    this.interval = setInterval(() => {
+      this.moveRight();
+    }, 5000);
+  }
+
+  moveLeft(): void {
+    if (this.currentIndex === 0) {
+      this.currentIndex = this.images.length - 4;
+    } else {
+      this.currentIndex -= 1;
+    }
+    this.updateVisibleImages();
+  }
+
+  moveRight(): void {
+    if (this.currentIndex === this.images.length - 4) {
+      this.currentIndex = 0;
+    } else {
+      this.currentIndex += 1;
+    }
+    this.updateVisibleImages();
+  }
+
+  updateVisibleImages(): void {
+    this.visibleImages = this.images.slice(this.currentIndex, this.currentIndex + 4);
+  }
+
 }
