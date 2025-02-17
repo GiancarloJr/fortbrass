7875d90a63150382f5554aee54af9e2579bfb846
GiancarloJr <108488305+GiancarloJr@users.noreply.github.com>
2025-02-17 20:33:01 -0300

    add: css job

--- src/app/jobs/jobs.component.ts
+++ src/app/jobs/jobs.component.ts
@@ -12,17 +12,17 @@ export class JobsComponent {
     {
       titulo: 'VAGA P/ ZELADOR EM JATAÍ - GO',
       descricao: '44 HORAS SEMANAIS<br>SALÁRIO DA CATEGORIA',
-      imagem: 'assets/zelador.jpg',
+      imagem: '../../assets/images/bombeiora.jpg',
     },
     {
       titulo: 'PORTEIRO',
       descricao: '40 HORAS SEMANAIS<br>R$ 1.341,45',
-      imagem: 'assets/porteiro.jpg',
+      imagem: '../../assets/images/portaria-01.jpg',
     },
     {
       titulo: 'AUXILIAR DE LIMPEZA',
       descricao: '40 HORAS SEMANAIS',
-      imagem: 'assets/auxiliar-limpeza.jpg',
+      imagem: '../../assets/images/fundo-01.jpg',
     }
   ];
 
